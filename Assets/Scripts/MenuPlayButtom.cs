bdf3accbe2fcb0b96b0793adbd5f6be1ce63af82
simbs38 <2k9andre@gmail.com>
2021-11-25 19:32:49 +0000

    Task 10 - Game Over menu implemented

--- Assets/Scripts/MenuPlayButtom.cs
+++ Assets/Scripts/MenuPlayButtom.cs
@@ -3,5 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuPlayButtom : MonoBehaviour
 {
+    public void GoToMainMenu() => SceneManager.LoadScene(0);
+
     public void GoToGamePlay() => SceneManager.LoadScene(1);
 }
