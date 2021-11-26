b471779ae4fa59f287ca45dee311a126775c1217
simbs38 <2k9andre@gmail.com>
2021-11-26 19:43:06 +0000

    Change space ship prefab organization

--- Assets/Scripts/ScreenTransport.cs
+++ Assets/Scripts/ScreenTransport.cs
@@ -1,28 +1,27 @@
 using UnityEngine;
 
-
 [RequireComponent(typeof(BoxCollider))]
 [RequireComponent(typeof(Rigidbody))]
 public class ScreenTransport : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(other.transform.parent == null || other.transform.parent.name != "Floor")
+        if (other.transform.parent == null || other.transform.parent.name != "Floor")
             return;
 
         Camera camera = Camera.main;
         Vector3 screenPosition = camera.WorldToScreenPoint(transform.position);
 
-        if(screenPosition.x < 0)
+        if (screenPosition.x < 0)
             screenPosition.x = Screen.width;
 
-        if(screenPosition.y < 0)
+        if (screenPosition.y < 0)
             screenPosition.y = Screen.height;
 
-        if(Screen.width < screenPosition.x)
+        if (Screen.width < screenPosition.x)
             screenPosition.x = 0;
 
-        if(Screen.height < screenPosition.y)
+        if (Screen.height < screenPosition.y)
             screenPosition.y = 0;
 
         Vector3 newPosition = camera.ScreenToWorldPoint(screenPosition);
